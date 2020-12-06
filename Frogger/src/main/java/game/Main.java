d556d3f1eb281d4aa1d77684f243a7eb30f33289
SnehaUmrit <hcysu1@nottingham.edu.my>
2020-12-06 14:55:35 +0400

    Removing unused files

--- Frogger/src/main/java/game/Main.java
+++ Frogger/src/main/java/game/Main.java
@@ -21,8 +21,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		MenuController.setMainStage(primaryStage);
-		MenuController.intialiseStage();
-	
+		MenuController.intialiseStage();	
 	}
 	
 }
